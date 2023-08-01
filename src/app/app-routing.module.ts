89c0d21b0b35a0fa1bec04db8c314dafd176ec43
Vignesh R <vignesh.r@3ktechnologies.com>
2023-08-01 14:43:54 +0530

    added routing with dummy components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CalenderComponent } from './components/calender/calender.component';
+import { MessagesComponent } from './components/messages/messages.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ProgressComponent } from './components/progress/progress.component';
+import { GoalsComponent } from './components/goals/goals.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", component: HomeComponent },
+  { path: "calender", component: CalenderComponent },
+  { path: "messages", component: MessagesComponent },
+  { path: "projects", component: ProjectsComponent, },
+  { path: "progress", component: ProgressComponent },
+  { path: "goals", component: GoalsComponent },
+  { path: "settings", component: SettingsComponent },
+  { path: "**", redirectTo: "home" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,13 @@ import { HeaderComponent } from './components/header/header.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MainBodyComponent } from './components/main-body/main-body.component';
 import { CardComponentComponent } from './components/card-component/card-component.component';
+import { HomeComponent } from './components/home/home.component';
+import { CalenderComponent } from './components/calender/calender.component';
+import { MessagesComponent } from './components/messages/messages.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ProgressComponent } from './components/progress/progress.component';
+import { GoalsComponent } from './components/goals/goals.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +21,14 @@ import { CardComponentComponent } from './components/card-component/card-compone
     HeaderComponent,
     NavbarComponent,
     MainBodyComponent,
-    CardComponentComponent
+    CardComponentComponent,
+    HomeComponent,
+    CalenderComponent,
+    MessagesComponent,
+    ProjectsComponent,
+    ProgressComponent,
+    GoalsComponent,
+    SettingsComponent,
   ],
   imports: [
     BrowserModule,
