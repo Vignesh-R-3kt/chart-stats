75f2af5cdedf79238976b2939ce73d85179a7c7c
Vignesh R <vignesh.r@3ktechnologies.com>
2023-08-07 18:04:03 +0530

    added modal popup

--- src/app/components/calender/calender.component.ts
+++ src/app/components/calender/calender.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./calender.component.scss']
 })
 export class CalenderComponent {
+  isPopupOpen: boolean = false;
 
+  openPopup() {
+    this.isPopupOpen = true;
+  }
+
+  closePopup() {
+    this.isPopupOpen = false;
+  }
 }
