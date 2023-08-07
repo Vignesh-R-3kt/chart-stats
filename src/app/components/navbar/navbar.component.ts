777749494469482db5ebc9c7d2fbd903e5561e58
VishalNMokashi <vishal.m@3ktechnologies.com>
2023-08-07 15:18:39 +0530

    Added drop down for profile

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
   panelOpenState: boolean = false;
+  dropdownOpen: boolean = false;
+  selectedDropdownItem: string = 'Profile-details';
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  selectDropdownItem(item: string): void {
+    this.selectedDropdownItem = item;
+    this.dropdownOpen = false;
+    console.log('Selected dropdown item:', this.selectedDropdownItem);
+  }
+
+  isItemSelected(item: string): boolean {
+    return this.selectedDropdownItem === item;
+  }
+
+  profileDetails() {
+    console.log('profile-details clicked');
+  }
+
+  logout() {
+    console.log('Logout profile clicked');
+  }
 }
