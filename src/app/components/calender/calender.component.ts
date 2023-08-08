07c5a1b336571121bbfcbb71d084ef17d41f8f96
Vignesh R <vignesh.r@3ktechnologies.com>
2023-08-08 14:04:27 +0530

    added form validation

--- src/app/components/calender/calender.component.ts
+++ src/app/components/calender/calender.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-calender',
@@ -8,6 +9,17 @@ import { Component } from '@angular/core';
 export class CalenderComponent {
   isPopupOpen: boolean = false;
   todayDate: Date = new Date();
+  startDate: Date = new Date();
+
+  constructor(private fb: FormBuilder) { }
+
+  projectForm = this.fb.group({
+    projectName: ["", [Validators.required, Validators.minLength(5)]],
+    projectOwner: ["", [Validators.required, Validators.minLength(5)]],
+    startDate: ["", Validators.required],
+    endDate: ["", Validators.required],
+    description: ["", [Validators.required, Validators.minLength(15)]]
+  });
 
   openPopup() {
     this.isPopupOpen = true;
@@ -15,5 +27,15 @@ export class CalenderComponent {
 
   closePopup() {
     this.isPopupOpen = false;
+  }
+
+  updateEndDate(e: any) {
+    const startDate = new Date(e.target.value);
+    this.startDate = startDate;
+  }
+
+  fetchFormData(e: any) {
+    e.preventDefault();
+    console.log(this.projectForm.value);
   }
 }
