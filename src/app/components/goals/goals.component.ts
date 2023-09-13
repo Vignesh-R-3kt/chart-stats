66cc2857c53940b18edf14cec7b5d3c28e6edad4
surekhatulasi <surekhatulasi1026@gmail.com>
2023-09-13 10:50:45 +0530

    Created Goals Component

--- src/app/components/goals/goals.component.ts
+++ src/app/components/goals/goals.component.ts
@@ -1,10 +1,121 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators ,AbstractControl} from '@angular/forms';
+import { ApiService } from 'src/app/services/api.service';
+
+function numberValidator(control: AbstractControl): { [key: string]: any } | null {
+  const value = control.value;
+  if (isNaN(value)) {
+    return { 'invalidNumber': true };
+  }
+  return null;
+}
 
 @Component({
   selector: 'app-goals',
   templateUrl: './goals.component.html',
   styleUrls: ['./goals.component.scss']
 })
-export class GoalsComponent {
-
-}
+export class GoalsComponent implements OnInit {
+  goals: any[] = [];
+  goalColors: any[]=[];
+  getGradientClass(index: number): string {
+    const gradientClasses = [
+      'linear-gradient-orange',
+      'linear-gradient-blue',
+      'linear-gradient-green',
+      'linear-gradient-purple'
+    ];
+    return gradientClasses[index % gradientClasses.length];
+  }
+  
+  colorIndex: number = 0;
+  isPopupOpen: boolean = false;
+  todayDate: Date = new Date();
+  startDate: Date = new Date();
+  goalFormVisible: boolean = false;
+  newGoal: any = {};
+  constructor(private fb: FormBuilder, private http: ApiService) { }
+  
+  
+  goalsForm = this.fb.group({
+    goalName: ["", [Validators.required, Validators.minLength(5)]],
+    Estimatedhours: ["", [Validators.required, Validators.minLength(1),numberValidator]],
+    startDate: ["", Validators.required],
+    endDate: ["", Validators.required],
+    description: ["", [Validators.required, Validators.minLength(15)]]
+  });
+  ngOnInit(): void {
+    this.fetchAllGoals();
+  }
+  fetchAllGoals() {
+    this.http.fetchAllGoals().subscribe((res: any) => {
+       this.goals = res; 
+    });
+  }
+  deleteGoal(id: number) {
+    this.http.deleteGoal(id).subscribe((res: any) => {
+      this.fetchAllGoals();
+    });
+  }
+  openPopup() {
+    this.isPopupOpen = true;
+    this.goalFormVisible = true;
+  }
+ 
+  closePopup() {
+    this.isPopupOpen = false;
+    this.goalFormVisible = false;
+  }
+  submitGoal() {
+    this.goalFormVisible = false;
+    this.newGoal.color = this.goalColors[this.colorIndex];
+    this.goals.push(this.newGoal);
+    this.colorIndex = (this.colorIndex + 1) % this.goalColors.length;
+    this.newGoal = {};
+   
+  }
+  
+  getCardColor(index: number): string {
+    
+    
+    if (index < 4) {
+      return this.goalColors[index];
+    } else {
+     
+      return this.goalColors[(index - 4) % this.goalColors.length];
+    }
+  }
+  updateEndDate(e: any) {
+    const startDate = new Date(e.target.value);
+    this.startDate = startDate;
+  }
+  fetchFormData(e: any) {
+    e.preventDefault();
+  
+    const formData = this.goalsForm.value;
+  
+    // Create a new goal object with form data
+    const newGoal = {
+      goalName: formData.goalName,
+      Estimatedhours: formData.Estimatedhours, // Make sure this line is correct
+      startDate: formData.startDate,
+      endDate: formData.endDate,
+      description: formData.description,
+      color: this.getCardColor(this.goals.length),
+    };
+  
+    if (this.goalsForm.valid) {
+      this.http.sendGoalToDB(newGoal).subscribe((res) => {
+      // Push the new goal to the goals array
+      this.goals.push(newGoal);
+      
+      // Clear the form
+      this.goalsForm.reset();
+  
+      // Close the popup
+      this.isPopupOpen = false;
+      this.goalFormVisible = false;
+    })
+    
+  }
+}  };
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,28 +1,41 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-
+ 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-
+ 
   baseUrl: string = "localhost:3000";
-
+ 
   constructor(private api: HttpClient) { }
-  
+ 
   // Method to fetch all data from DB
   fetchAllData() {
     return this.api.get(`http://${this.baseUrl}/projects`);
   }
-
+ 
    // Method to add new project details to DB
   sendDataToDB(data: any) {
     let options = { headers: new HttpHeaders().set('Content-Type', 'application/json') };
     return this.api.post(`http://${this.baseUrl}/projects`, JSON.stringify(data), options);
   }
-
+ 
   // Method to delete the item in DB using Id number
   deleteData(id: number) {
     return this.api.delete(`http://${this.baseUrl}/projects/${id}`);
+  }
+  
+  fetchAllGoals() {
+    return this.api.get(`http://${this.baseUrl}/goals`);
+  }
+ 
+  sendGoalToDB(goal: any) {
+    let options = { headers: new HttpHeaders().set('Content-Type', 'application/json') };
+    return this.api.post(`http://${this.baseUrl}/goals`, JSON.stringify(goal), options);
+  }
+ 
+  deleteGoal(id: number) {
+    return this.api.delete(`http://${this.baseUrl}/goals/${id}`);
   }
 }
