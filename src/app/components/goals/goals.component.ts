bb3f202dc9dcc11b8a9ac583d704b6a3b200948e
surekhatulasi <surekhatulasi1026@gmail.com>
2023-09-13 14:50:09 +0530

    Used flex to display the cards & Formatted the Code

--- src/app/components/goals/goals.component.ts
+++ src/app/components/goals/goals.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators ,AbstractControl} from '@angular/forms';
+import { FormBuilder, Validators, AbstractControl } from '@angular/forms';
 import { ApiService } from 'src/app/services/api.service';
 
-function numberValidator(control: AbstractControl): { [key: string]: any } | null {
+function numberValidator(
+  control: AbstractControl
+): { [key: string]: any } | null {
   const value = control.value;
   if (isNaN(value)) {
-    return { 'invalidNumber': true };
+    return { invalidNumber: true };
   }
   return null;
 }
@@ -13,43 +15,45 @@ function numberValidator(control: AbstractControl): { [key: string]: any } | nul
 @Component({
   selector: 'app-goals',
   templateUrl: './goals.component.html',
-  styleUrls: ['./goals.component.scss']
+  styleUrls: ['./goals.component.scss'],
 })
 export class GoalsComponent implements OnInit {
   goals: any[] = [];
-  goalColors: any[]=[];
+  goalColors: any[] = [];
   getGradientClass(index: number): string {
     const gradientClasses = [
       'linear-gradient-orange',
       'linear-gradient-blue',
       'linear-gradient-green',
-      'linear-gradient-purple'
+      'linear-gradient-purple',
     ];
     return gradientClasses[index % gradientClasses.length];
   }
-  
+
   colorIndex: number = 0;
   isPopupOpen: boolean = false;
   todayDate: Date = new Date();
   startDate: Date = new Date();
   goalFormVisible: boolean = false;
   newGoal: any = {};
-  constructor(private fb: FormBuilder, private http: ApiService) { }
-  
-  
+  constructor(private fb: FormBuilder, private http: ApiService) {}
+
   goalsForm = this.fb.group({
-    goalName: ["", [Validators.required, Validators.minLength(5)]],
-    Estimatedhours: ["", [Validators.required, Validators.minLength(1),numberValidator]],
-    startDate: ["", Validators.required],
-    endDate: ["", Validators.required],
-    description: ["", [Validators.required, Validators.minLength(15)]]
+    goalName: ['', [Validators.required, Validators.minLength(5)]],
+    Estimatedhours: [
+      '',
+      [Validators.required, Validators.minLength(1), numberValidator],
+    ],
+    startDate: ['', Validators.required],
+    endDate: ['', Validators.required],
+    description: ['', [Validators.required, Validators.minLength(15)]],
   });
   ngOnInit(): void {
     this.fetchAllGoals();
   }
   fetchAllGoals() {
     this.http.fetchAllGoals().subscribe((res: any) => {
-       this.goals = res; 
+      this.goals = res;
     });
   }
   deleteGoal(id: number) {
@@ -61,7 +65,7 @@ export class GoalsComponent implements OnInit {
     this.isPopupOpen = true;
     this.goalFormVisible = true;
   }
- 
+
   closePopup() {
     this.isPopupOpen = false;
     this.goalFormVisible = false;
@@ -72,16 +76,12 @@ export class GoalsComponent implements OnInit {
     this.goals.push(this.newGoal);
     this.colorIndex = (this.colorIndex + 1) % this.goalColors.length;
     this.newGoal = {};
-   
   }
-  
+
   getCardColor(index: number): string {
-    
-    
     if (index < 4) {
       return this.goalColors[index];
     } else {
-     
       return this.goalColors[(index - 4) % this.goalColors.length];
     }
   }
@@ -91,9 +91,9 @@ export class GoalsComponent implements OnInit {
   }
   fetchFormData(e: any) {
     e.preventDefault();
-  
+
     const formData = this.goalsForm.value;
-  
+
     // Create a new goal object with form data
     const newGoal = {
       goalName: formData.goalName,
@@ -103,19 +103,19 @@ export class GoalsComponent implements OnInit {
       description: formData.description,
       color: this.getCardColor(this.goals.length),
     };
-  
+
     if (this.goalsForm.valid) {
       this.http.sendGoalToDB(newGoal).subscribe((res) => {
-      // Push the new goal to the goals array
-      this.goals.push(newGoal);
-      
-      // Clear the form
-      this.goalsForm.reset();
-  
-      // Close the popup
-      this.isPopupOpen = false;
-      this.goalFormVisible = false;
-    })
-    
+        // Push the new goal to the goals array
+        this.goals.push(newGoal);
+
+        // Clear the form
+        this.goalsForm.reset();
+
+        // Close the popup
+        this.isPopupOpen = false;
+        this.goalFormVisible = false;
+      });
+    }
   }
-}  };
+}
