02b99aeb75fd44ab09af4199343674df3e456558
Vignesh R <vignesh.r@3ktechnologies.com>
2023-09-14 14:31:40 +0530

    added bar chart to progress tab

--- src/app/components/progress/progress.component.ts
+++ src/app/components/progress/progress.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styleUrls: ['./progress.component.scss']
+  styleUrls: ['./progress.component.scss'],
 })
-export class ProgressComponent {
+export class ProgressComponent implements OnInit {
+  ngOnInit(): void {
+    this.generateChart();
+  }
 
+  generateChart() {
+    const options: Highcharts.Options = {
+      chart: {
+        type: 'column',
+        backgroundColor: '#2B2B4B',
+      },
+      title: {
+        text: 'Projects | Year wise',
+      },
+      xAxis: {
+        categories: ['2018', '2019', '2020', '2021', '2022', '2023'],
+        title: {
+          text: 'Year',
+          style: {
+            fontSize: "18px",
+          }
+        },
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: 'No of Projects',
+        },
+      },
+      tooltip: {
+        backgroundColor: "#000033"
+      },
+      plotOptions: {
+        column: {
+          pointPadding: 0.2,
+          borderWidth: 0,
+        },
+      },
+      series: [
+        {
+          type: 'column',
+          name: 'No of Projects',
+          data: [100, 150, 80, 60, 160, 50],
+        },
+      ],
+    };
+
+    Highcharts.chart('progress-container', options);
+  }
 }
