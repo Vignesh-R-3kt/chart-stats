4d16f1f3441301dc748e27c41cf173430efafc5e
VishalNMokashi <vishal.m@3ktechnologies.com>
2023-09-22 10:25:49 +0530

    added team and tracking tabs

--- src/app/components/project-details/estimates/estimates.component.ts
+++ src/app/components/project-details/estimates/estimates.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+import { estimates } from '../project.modal';
 
 @Component({
   selector: 'app-estimates',
   templateUrl: './estimates.component.html',
   styleUrls: ['./estimates.component.scss']
 })
-export class EstimatesComponent {
+export class EstimatesComponent implements OnInit {
 
+  projects: any
+  tableData: any[] = [];
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit():void {
+   this.getEstimatesData();
+  }
+
+  // Method to fetch all data table in estimates 
+  getEstimatesData() {
+    this.apiService.getEstimatesData().subscribe((data) => {
+      this.tableData = data.map((item: any) => {
+        return {
+          id: item.id,
+          name: item.name,
+          date: new Date(item.date).toLocaleDateString(),
+          hours: item.hours,
+          cost: item.cost
+        };
+      });
+
+      $('#projectsTable').DataTable().destroy();
+      setTimeout(() => {
+        $('#projectsTable').DataTable({
+          pagingType: 'full_numbers',
+          processing: true,
+        });
+      }, 1);
+    });
+  }
 }
